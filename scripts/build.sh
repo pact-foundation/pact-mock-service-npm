c1fc45b63f2f839d826d799e59264800407d262d
Beth <beth@bethesque.com>
2015-05-07 17:18:04 +1000

    Added script to package windows

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,6 +6,11 @@ export PACKAGE_VERSION=${GEM_VERSION}-${RELEASE_VERSION}
 
 scripts/build_standalone_packages.sh
 
+export STANDALONE_PACKAGE_NAME="pact-mock-service-$PACKAGE_VERSION-win"
+export NPM_PACKAGE_NAME='pact-mock-service-win'
+export SUFFIX='win'
+scripts/package_for_npm.sh
+
 export STANDALONE_PACKAGE_NAME="pact-mock-service-$PACKAGE_VERSION-osx"
 export NPM_PACKAGE_NAME='pact-mock-service-osx'
 export SUFFIX='osx'
