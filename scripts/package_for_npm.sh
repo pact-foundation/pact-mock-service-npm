3ae0147af9dfb4095dfda622073a16c34ad58434
Anthony Damtsis <adamtsis@dius.com.au>
2015-05-08 22:52:38 +1000

    Added packaging support for zip files for windows

--- scripts/package_for_npm.sh
+++ scripts/package_for_npm.sh
@@ -4,9 +4,19 @@ echo "Packaging $STANDALONE_PACKAGE_NAME.tar.gz for npm as $NPM_PACKAGE_NAME.tar
 mkdir -p dist
 cd build
 cp pact-mock_service/pkg/pact-mock-service-*.tar.gz .
+cp pact-mock_service/pkg/pact-mock-service-*.zip .
 rm -rf pact-mock-service-osx
-tar -xzf $STANDALONE_PACKAGE_NAME.tar.gz
+if [ $EXTENSION = "zip" ]; then
+  echo "unzip $STANDALONE_PACKAGE_NAME.zip"
+  unzip $STANDALONE_PACKAGE_NAME.zip
+else
+  tar -xzf $STANDALONE_PACKAGE_NAME.$EXTENSION
+fi
 cp ../$SUFFIX/package.json $STANDALONE_PACKAGE_NAME
 cp ../$SUFFIX/README.md $STANDALONE_PACKAGE_NAME
 cd $STANDALONE_PACKAGE_NAME
-tar -czf ../../dist/$NPM_PACKAGE_NAME.tar.gz .
+if [ $EXTENSION = "zip" ]; then
+  zip -9r ../../dist/$NPM_PACKAGE_NAME.zip .
+else
+  tar -czf ../../dist/$NPM_PACKAGE_NAME.$EXTENSION .
+fi
