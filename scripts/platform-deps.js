5865da3459eb933d7d11c8f02eed5f8b500b1532
Michel Boudreau <michelb@pageuppeople.com>
2016-01-06 10:55:19 +1100

    using exec (async) instead of execSync to be able to catch errors

--- scripts/platform-deps.js
+++ scripts/platform-deps.js
@@ -1,4 +1,4 @@
-var exec = require('child_process').execSync,
+var exec = require('child_process').exec,
 	path = require('path'),
 	fs = require('fs'),
 	pkgPath = path.resolve(__dirname, '../package.json'),
@@ -12,20 +12,13 @@ if (process.platform === 'linux') {
 var packageName = 'pact-mock-service-' + process.platform + arch;
 console.log("Installing Pact mock server for " + packageName);
 
-// Install standalone package to node_modules, use exec to prevent --save or --save-dev flags to be sent.  DON'T USE `npm.commands.install`
-exec('npm install ' + packageName + '@' + pkg.version, {cwd: path.resolve(__dirname, '..')});
+// Install standalone package to node_modules, use exec to prevent --save or --save-dev flags to be used if user inputted.  DON'T USE `npm.commands.install`
+exec('npm install ' + packageName + '@' + pkg.version, {cwd: path.resolve(__dirname, '..')}, function(error, stdout, stderr){
+	if (error) {
+		console.error(error);
+		process.exit(1);
+	}
+	console.log("Pact mock server for " + packageName + " installed successfully.");
+});
 
-// if (error) {
-// 	console.error(error);
-// 	process.exit(1);
-// }
 
-// Specify the bin path of the downloaded package in the package.json of pact-mock-service
-var p = require.resolve(packageName);
-// Setting path to be relative to project's package.json
-pkg.bin = {'pact-mock-service': './' + path.relative(path.dirname(pkgPath), p)/*.split(path.sep).join('/')*/};
-
-// Write it to package.json
-fs.writeFileSync(pkgPath, JSON.stringify(pkg, null, 2));
-
-console.log("Pact mock server for " + packageName + " installed successfully.");
