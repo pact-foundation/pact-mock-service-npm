f66b92fc71670b77bf997603bd20e1d3f4d088ba
Michel Boudreau <michelboudreau@gmail.com>
2016-04-29 01:03:28 +1000

    removing cloning and using zip files instead

--- scripts/build_standalone_packages.sh
+++ scripts/build_standalone_packages.sh
@@ -4,17 +4,14 @@ set -e
 
 mkdir -p build
 cd build
-if [ ! -d "pact-mock_service" ]; then
-  git clone git@github.com:bethesque/pact-mock_service.git
-  cd pact-mock_service
-else
-  cd pact-mock_service
-  git checkout master
-  git pull origin master
-  git fetch --tags
+if [ -d "pact-mock_service" ]; then
+  rm -R pact-mock_service
 fi
 
-git checkout tags/v${GEM_VERSION}
+wget https://github.com/bethesque/pact-mock_service/archive/v${GEM_VERSION}.zip -O temp.zip
+unzip temp.zip
+cd pact-mock_service-${GEM_VERSION}
+
 
 bundle
 bundle exec rake package
